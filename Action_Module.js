552622810a70a2f435f2b9874cc60363cc61f4e3
NeilRiver <41006652+NeilRiver@users.noreply.github.com>
2020-08-07 15:36:05 +0300

    update 07.08.2020
    inject bootstrap
    fix bootstrap
    
    implemented dropdown-menu + list

--- Action_Module.js
+++ Action_Module.js
@@ -3,6 +3,29 @@ var tempGlobal;
 let arrayIdValues = [];
 let arrayElements = [];
 
+const bootstrapButton = (id) => {
+    return `
+<div class="btn-group">
+  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    ${id}
+  </button>
+  <div class="dropdown-menu">
+  <a class="dropdown-item" href="#">Заявка выполнена</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="#">на Колесника</a>
+    <a class="dropdown-item" href="#">на Драба</a>
+    <a class="dropdown-item" href="#">на Юрича</a>
+    <a class="dropdown-item" href="#">на Викторовича</a>
+    <a class="dropdown-item" href="#">на Колю</a>
+    <a class="dropdown-item" href="#">на Антона</a>
+    <a class="dropdown-item" href="#">на Диму</a>
+  </div>
+</div>
+`
+};
+
+
+
 //url: "http://servicedesk:8080/DynamicNotification.do?method=getDynamicNotificationCount",
 
 const findElementbyValue = (value, arr) => {
@@ -49,14 +72,14 @@ for (let key in emptyDivs1.prevObject) {
     if (key === 'length') {
         break
     } else {
-        (emptyDivs1.prevObject[key].outerHTML.match(/value=\"[0-9]+\"/gm)) ? arrayIdValues.push(emptyDivs1.prevObject[key]) : null
+        (emptyDivs1.prevObject[key].outerHTML.match(/value=\"[0-9]+\"/gm)) ? arrayIdValues.push(emptyDivs1.prevObject[key]): null
     }
 }
 for (let key in emptyDivs2.prevObject) {
     if (key === 'length') {
         break
     } else {
-        (emptyDivs2.prevObject[key].outerHTML.match(/value=\"[0-9]+\"/gm)) ? arrayIdValues.push(emptyDivs2.prevObject[key]) : null
+        (emptyDivs2.prevObject[key].outerHTML.match(/value=\"[0-9]+\"/gm)) ? arrayIdValues.push(emptyDivs2.prevObject[key]): null
     }
 }
 
@@ -89,3 +112,10 @@ arrayElements = arrayElements.sort((a, b) => a.innerText < b.innerText)
 
 console.log(arrayIdValues)
 console.log(arrayElements)
+
+arrayElements.forEach((v, i) => {
+
+    console.log(v, i)
+    $(v).after(bootstrapButton(v.textContent)).remove()
+
+})
